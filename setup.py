58f83cbcfbb51dec5cdb75084dec2181e24f3197
Opher Lieber <opherlie@gmail.com>
2019-09-05 10:49:31 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from setuptools import setup
 setup(
     name='rltime',
     version='0.1',
+    author="Opher Lieber",
+    author_email="opherlie@gmail.com",
     install_requires=[
         'gym',
         'cloudpickle',
         'opencv-python',
     ],
     packages=["rltime"],
+    python_requires='>=3.6'
 )
