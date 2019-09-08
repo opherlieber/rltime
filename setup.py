2f0713254398832f3e710a63763886cf353b5c8a
Opher Lieber <opherlie@gmail.com>
2019-09-08 12:35:13 +0300

    Updated description and version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rltime',
-    version='0.1',
+    version='0.1.1',
     author="Opher Lieber",
     author_email="opherlie@gmail.com",
     install_requires=[
@@ -11,5 +11,11 @@ setup(
         'opencv-python',
     ],
     packages=["rltime"],
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    description="RLtime is a reinforcement learning library focused on state-of-the-art q-learning algorithms and features",
+    long_description="""
+RLtime is a reinforcement learning library, currently supporting PyTorch, with focus on state-of-the-art q-learning algorithms and features, and interacting with real-time environments which require low-latency acting and sample-efficient training.
+The latest code and instructions can be found here:
+https://github.com/opherlieber/rltime
+"""
 )
